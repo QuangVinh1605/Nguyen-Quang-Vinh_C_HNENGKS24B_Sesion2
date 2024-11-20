da35f93936e14788ac6f6d4da875d542ef8dd962
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-20 20:48:12 +0700

    upload baitap C ss2-05

---
+++ C.SS2.5.cpp
@@ -0,0 +1,14 @@
+#include (stdio.h)
+
+int main(){
+	int rectangule_length;
+	int rectangule_ length=4;
+	int rectagule_width;
+	int rectagule_width=5;
+	int rectangle_area=rectangule_width*rectangule_length;
+	int rectangle_perimeter_half=rectangule_width+rectangule_length;
+	int rectangle_perimeter=rectangle_perimeter_half*2;
+	return=o; 
+}
+
+ 

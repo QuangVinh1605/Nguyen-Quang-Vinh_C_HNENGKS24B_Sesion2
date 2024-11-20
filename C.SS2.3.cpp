e3166d9796c974f18ccf83be1bdee3a6d86697e2
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-20 20:47:37 +0700

    upload baitap C ss2-03

---
+++ C.SS2.3.cpp
@@ -0,0 +1,16 @@
+#include (stdio.h)
+
+int main(){
+	int num1=16;
+	int num2=10;
+	int result;
+	int result=num1+num2;
+	int subtraction;
+	int subtraction= num1-num2;
+	int multiplication;
+	int multiplication=num1*num2;
+	int division;
+	int division=num1/num2;
+	return 0;
+  
+} 

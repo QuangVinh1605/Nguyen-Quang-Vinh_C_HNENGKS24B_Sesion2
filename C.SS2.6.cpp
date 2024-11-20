8ad6642fe87d145c399eed02faa67757e6a532f7
QuangVinh1605 <nhv16052006@gmail.com>
2024-11-20 20:48:32 +0700

    upload baitap C ss2-06

---
+++ C.SS2.6.cpp
@@ -0,0 +1,10 @@
+#include (stdio.h)
+
+float main(){
+	float pI=3,14;
+	int circle_radius=4;
+	float circle_perimeter=2*pI*circle_radius;
+	float circle_area=pI*circle_radius*circle_radius;
+	return=0;
+	 
+}
